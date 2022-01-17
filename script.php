cb5372f805a6b6d01cdfb90c1d61890392ae3437
Joomill <info@joomill.nl>
2022-01-17 10:22:15 +0100

    V4.0.2

--- script.php
+++ script.php
@@ -1,8 +1,8 @@
 <?php
 /*
  *  package: Reset Hits module - FREE Version
- *  copyright: Copyright (c) 2021. Jeroen Moolenschot | Joomill
- *  license: GNU General Public License version 3 or later
+ *  copyright: Copyright (c) 2022. Jeroen Moolenschot | Joomill
+ *  license: GNU General Public License version 2 or later
  *  link: https://www.joomill-extensions.com
  */
 
