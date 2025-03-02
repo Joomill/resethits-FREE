f1fbdbf5541cee7c83bebbaa41de6c5134bed7e8
JeroenMoolenschot <info@joomill.nl>
2025-03-02 16:12:47 +0100

    v4.2.0 - Install Script and auto publish module

--- script.php
+++ script.php
@@ -17,7 +17,9 @@ use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Table\Table;
 
 /**
- * Load the Reset Hits module installer
+ * Script file of Reset Hits module
+ *
+ * @since  4.0.0
  */
 class mod_ResethitsInstallerScript
 {
@@ -71,8 +73,137 @@ class mod_ResethitsInstallerScript
         }
         return true;
     }
+
+	/**
+	 * Function called during the installation of the extension
+	 *
+	 * @param InstallerAdapter $parent The class calling this method
+	 * @return boolean True on success
+	 * @since 4.0.0
+	 */
+	public function install($parent)
+	{
+		// Enable the extension
+		$this->enableModule();
+
+		return true;
+	}
+
+	/**
+	 * Enable the module by publishing it and assigning it to the cpanel position
+	 *
+	 * @return void
+	 * @since  4.0.0
+	 */
+	private function enableModule()
+	{
+		// Check if Module has not been published yet
+		$db    = Factory::getContainer()->get('DatabaseDriver');
+		$query = $db->getQuery(true);
+		$query->select($db->quoteName('id'));
+		$query->from($db->quoteName('#__modules'));
+		$query->where($db->quoteName('module') . ' = ' . $db->quote('mod_resethits'));
+		$query->where($db->quoteName('published') . ' = 1');
+		$query->where($db->quoteName('position') . ' = ' . $db->quote('cpanel'));
+		$db->setQuery($query);
+		$moduleId = $db->loadResult();
+
+		// If the Module has not been published, publish + assign it
+
+		if (empty($moduleId))
+		{
+			// Change Module settings to auto publish it on position cpanel
+			$query      = $db->getQuery(true);
+			$fields     = array(
+				$db->quoteName('title') . ' = ' . $db->quote('Reset Hits module'),
+				$db->quoteName('published') . ' = 1',
+				$db->quoteName('position') . ' = ' . $db->quote('cpanel'),
+				$db->quoteName('access') . ' = 3',
+				$db->quoteName('params') . ' = ' . $db->quote('{"articlehits":1,"articlerevisions":1,"bannerimpressions":1,"bannerclicks":1,"userpassword":1,
+				"layout":"_:default","header_icon":"fa-solid fa-arrow-rotate-left","module_tag":"div","bootstrap_size":"0","header_tag":"h2","header_class":"","style":"0"}'),
+			);
+			$conditions = array($db->quoteName('module') . ' = ' . $db->quote('mod_resethits'));
+			$query->update($db->quoteName('#__modules'))->set($fields)->where($conditions);
+			$db->setQuery($query);
+			$db->execute();
+
+			// Get ID for module
+			$query = $db->getQuery(true);
+			$query->select($db->quoteName('id'));
+			$query->from($db->quoteName('#__modules'));
+			$query->where($db->quoteName('module') . ' = ' . $db->quote('mod_resethits'));
+			$db->setQuery($query);
+			$moduleId = $db->loadResult();
+
+			// Add to modules_menu
+			$query  = $db->getQuery(true);
+			$fields = array(
+				$db->quoteName('moduleid') . ' = ' . $db->quote($moduleId),
+				$db->quoteName('menuid') . ' = 0',
+			);
+
+			$query->insert($db->quoteName('#__modules_menu'))->set($fields);
+			$db->setQuery($query);
+			$db->execute();
+		}
+	}
+
+	/**
+	 * Function called after extension installation/update/removal procedure commences
+	 *
+	 * @param   string            $type    The type of change (install, update or discover_install, not uninstall)
+	 * @param   InstallerAdapter  $parent  The class calling this method
+	 *
+	 * @return  boolean  True on success
+	 * @since  4.0.0
+	 */
+	public function postflight($type, $parent)
+	{
+		if ($type === 'install')
+		{
+			echo '<style>a[target="_blank"]::before {display: none};</style>';
+			echo '<div class="mb-3 text-center"><img src="https://www.joomill-extensions.com/images/joomill-logo.png" alt="Joomill Extensions" /></div>';
+			echo '<div class="mb-3 text-center"><strong>' . Text::_('MOD_RESETHITS_XML_DESCRIPTION') . '</strong></div>';
+			echo '<h3 class="text-center">' . Text::_('MOD_RESETHITS_THANKYOU') . '</h3>';
+			echo '<br>';
+			echo '<h3>' . Text::_('MOD_RESETHITS_QUICKSTART') . ':</h3>';
+			echo '<ul>';
+			echo '<li><a style="text-decoration: underline;" href="index.php" target="_blank">' . Text::_('MOD_RESETHITS_INSTALL_CONFIGURATION') . '</a></li>';
+			echo '<li><a style="text-decoration: underline;" href="https://www.joomill-extensions.com/documentation/reset-hits-module" target="_blank">' . Text::_('MOD_RESETHITS_INSTALL_NEEDHELP') . '</a></li>';
+			echo '</ul>';
+			echo '<hr>';
+			echo '<div class="text-center">' . Text::_('MOD_RESETHITS_FOLLOWME') . ':</div>';
+			echo '<div class="text-center">';
+			echo '<a class="m-2" href="https://www.linkedin.com/in/jeroenmoolenschot/" target="_blank"><i class="fa-brands fa-linkedin"> </i></a>';
+			echo '<a class="m-2" href="https://www.facebook.com/Joomill" target="_blank"><i class="fa-brands fa-facebook-f"> </i></a>';
+			echo '<a class="m-2" href="https://www.instagram.com/Joomill" target="_blank"><i class="fa-brands fa-instagram"> </i></a>';
+			echo '<a class="m-2" href="https://bsky.app/profile/joomill.bsky.social" target="_blank"><i class="fa-brands fa-bluesky"> </i></a>';
+			echo '<a class="m-2" href="https://joomla.social/@joomill" target="_blank"><i class="fa-brands fa-mastodon"></i> </i></a>';
+			echo '<a class="m-2" href="https://www.threads.net/@joomill" target="_blank"><i class="fa-brands fa-threads"></i> </i></a>';
+			echo '<a class="m-2" href="https://www.twitter.com/Joomill" target="_blank"><i class="fa-brands fa-brands fa-x-twitter"> </i></a>';
+			echo '<a class="m-2" href="https://community.joomla.org/service-providers-directory/listings/67:joomill.html" target="_blank"><i class="fa-brands fa-joomla"> </i></a>';
+			echo '</div>';
+		}
+		if ($type === 'uninstall')
+		{
+			echo '<style>a[target="_blank"]::before {display: none};</style>';
+			echo '<div class="mb-3 text-center"><img src="https://www.joomill-extensions.com/images/joomill-logo.png" alt="Joomill Extensions" /></div>';
+			echo '<br>';
+			echo '<h3 class="text-center">' . Text::_('MOD_RESETHITS_THANKYOU') . '</h3>';
+			echo '<br>';
+			echo '<div class="text-center">' . Text::_('MOD_RESETHITS_FOLLOWME') . ':</div>';
+			echo '<div class="text-center">';
+			echo '<a class="m-2" href="https://www.linkedin.com/in/jeroenmoolenschot/" target="_blank"><i class="fa-brands fa-linkedin"> </i></a>';
+			echo '<a class="m-2" href="https://www.facebook.com/Joomill" target="_blank"><i class="fa-brands fa-facebook-f"> </i></a>';
+			echo '<a class="m-2" href="https://www.instagram.com/Joomill" target="_blank"><i class="fa-brands fa-instagram"> </i></a>';
+			echo '<a class="m-2" href="https://bsky.app/profile/joomill.bsky.social" target="_blank"><i class="fa-brands fa-bluesky"> </i></a>';
+			echo '<a class="m-2" href="https://joomla.social/@joomill" target="_blank"><i class="fa-brands fa-mastodon"></i> </i></a>';
+			echo '<a class="m-2" href="https://www.threads.net/@joomill" target="_blank"><i class="fa-brands fa-threads"></i> </i></a>';
+			echo '<a class="m-2" href="https://www.twitter.com/Joomill" target="_blank"><i class="fa-brands fa-brands fa-x-twitter"> </i></a>';
+			echo '<a class="m-2" href="https://community.joomla.org/service-providers-directory/listings/67:joomill.html" target="_blank"><i class="fa-brands fa-joomla"> </i></a>';
+			echo '</div>';
+		}
+
+		return true;
+	}
 }
-
-
-		
-
